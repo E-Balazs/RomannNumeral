c17049ce83a85068b95ba6af003cd35d6cffb174
E-Balazs <57632206+E-Balazs@users.noreply.github.com>
2021-06-25 17:36:28 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Rómaiváltó
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BT_Merre = new System.Windows.Forms.Button();
+            this.BT_Vált = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TB_A = new System.Windows.Forms.TextBox();
+            this.TB_R = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BT_Merre
+            // 
+            this.BT_Merre.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BT_Merre.Location = new System.Drawing.Point(220, 90);
+            this.BT_Merre.Name = "BT_Merre";
+            this.BT_Merre.Size = new System.Drawing.Size(75, 58);
+            this.BT_Merre.TabIndex = 0;
+            this.BT_Merre.Text = "→";
+            this.BT_Merre.UseVisualStyleBackColor = true;
+            this.BT_Merre.Click += new System.EventHandler(this.BT_Merre_Click);
+            // 
+            // BT_Vált
+            // 
+            this.BT_Vált.Location = new System.Drawing.Point(220, 180);
+            this.BT_Vált.Name = "BT_Vált";
+            this.BT_Vált.Size = new System.Drawing.Size(75, 38);
+            this.BT_Vált.TabIndex = 1;
+            this.BT_Vált.Text = "Vált";
+            this.BT_Vált.UseVisualStyleBackColor = true;
+            this.BT_Vált.Click += new System.EventHandler(this.BT_Vált_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(62, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Arab";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(405, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Római";
+            // 
+            // TB_A
+            // 
+            this.TB_A.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TB_A.Location = new System.Drawing.Point(16, 126);
+            this.TB_A.Name = "TB_A";
+            this.TB_A.Size = new System.Drawing.Size(198, 30);
+            this.TB_A.TabIndex = 4;
+            this.TB_A.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TB_A_KeyPress);
+            // 
+            // TB_R
+            // 
+            this.TB_R.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TB_R.Location = new System.Drawing.Point(301, 126);
+            this.TB_R.Name = "TB_R";
+            this.TB_R.Size = new System.Drawing.Size(198, 30);
+            this.TB_R.TabIndex = 5;
+            this.TB_R.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TB_R_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 312);
+            this.Controls.Add(this.TB_R);
+            this.Controls.Add(this.TB_A);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BT_Vált);
+            this.Controls.Add(this.BT_Merre);
+            this.Name = "Form1";
+            this.Text = "Váltó";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BT_Merre;
+        private System.Windows.Forms.Button BT_Vált;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TB_A;
+        private System.Windows.Forms.TextBox TB_R;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rómaiváltó
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool oda = true;
+
+        private void BT_Merre_Click(object sender, EventArgs e)
+        {
+            oda = !oda;
+            if (oda)
+            {
+                BT_Merre.Text = "→";
+            }
+            else
+            {
+                BT_Merre.Text = "←";
+            }
+        }
+
+        private void BT_Vált_Click(object sender, EventArgs e)
+        {
+            Váltó izé = new Rómaiváltó.Váltó();
+            if (oda)
+            {
+                int a;
+                int.TryParse(TB_A.Text,out a);
+                TB_R.Text = izé.Rómaira(a);
+            }
+            else
+            {
+                int lel = 0;
+                lel = izé.Arabra(TB_R.Text,ref lel);
+                TB_A.Text = lel.ToString();
+            }
+        }
+
+        private void TB_A_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void TB_R_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           char[] római = { 'I', 'V', 'X', 'L', 'C', 'D', 'M', 'ↁ', 'ↂ'};
+            e.Handled = !római.Contains(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }
+
+    class Váltó
+    {
+        public string Rómaira(int mit)
+        {
+            if (mit >= 10000)
+            {
+                return "ↂ" + Rómaira(mit - 10000);
+            }
+            if (mit >= 9000)
+            {
+                return "Mↂ" + Rómaira(mit - 9000);
+            }
+            if (mit >= 5000)
+            {
+                return "ↁ" + Rómaira(mit - 5000);
+            }
+            if (mit >= 4000)
+            {
+                return "Mↁ" + Rómaira(mit - 4000);
+            }
+            if (mit >= 1000)
+            {
+                return "M" + Rómaira(mit - 1000);
+            }
+            if (mit >= 900)
+            {
+                return "CM" + Rómaira(mit - 900);
+            }
+            if (mit >= 500)
+            {
+                return "D" + Rómaira(mit - 500);
+            }
+            if (mit >= 400)
+            {
+                return "CD" + Rómaira(mit - 400);
+            }
+            if (mit >= 100)
+            {
+                return "C" + Rómaira(mit - 100);
+            }
+            if (mit >= 90)
+            {
+                return "XC" + Rómaira(mit - 90);
+            }
+            if (mit >= 50)
+            {
+                return "L" + Rómaira(mit - 50);
+            }
+            if (mit >= 40)
+            {
+                return "XL" + Rómaira(mit - 40);
+            }
+            if (mit >= 10)
+            {
+                return "X" + Rómaira(mit - 10);
+            }
+            if (mit >= 9)
+            {
+                return "IX" + Rómaira(mit - 9);
+            }
+            if (mit >= 5)
+            {
+                return "V" + Rómaira(mit - 5);
+            }
+            if (mit >= 4)
+            {
+                return "IV" + Rómaira(mit - 4);
+            }
+            if (mit >= 1)
+            {
+                return "I" + Rómaira(mit - 1);
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public int Arabra(string mit, ref int mire)
+        {
+            if (mit.Length > 0)
+            {
+                switch (mit[0])
+                {
+                    case 'ↂ':
+                        mire += 10000;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                    case 'ↁ':
+                        mire += 5000;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                    case 'M':
+                        if (mit.Length > 1)
+                        {
+                            switch (mit[1])
+                            {
+                                case 'ↂ':
+                                    mire += 9000;
+                                    return Arabra(mit.Remove(0, 2), ref mire);
+                                case 'ↁ':
+                                    mire += 4000;
+                                    return Arabra(mit.Remove(0, 2), ref mire);
+                                default:
+                                    mire += 1000;
+                                    return Arabra(mit.Remove(0, 1), ref mire);
+                            }
+                        }
+                        mire += 1000;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                    case 'D':
+                        mire += 500;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                    case 'C':
+                        if (mit.Length > 1)
+                        {
+                            switch (mit[1])
+                            {
+                                case 'M':
+                                    mire += 900;
+                                    return Arabra(mit.Remove(0, 2), ref mire);
+                                case 'D':
+                                    mire += 400;
+                                    return Arabra(mit.Remove(0, 2), ref mire);
+                                default:
+                                    mire += 100;
+                                    return Arabra(mit.Remove(0, 1), ref mire);
+                            }
+                        }
+                        mire += 100;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                    case 'L':
+                        mire += 50;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                    case 'X':
+                        if (mit.Length > 1)
+                        {
+                            switch (mit[1])
+                            {
+                                case 'C':
+                                    mire += 90;
+                                    return Arabra(mit.Remove(0, 2), ref mire);
+                                case 'L':
+                                    mire += 40;
+                                    return Arabra(mit.Remove(0, 2), ref mire);
+                                default:
+                                    mire += 10;
+                                    return Arabra(mit.Remove(0, 1), ref mire);
+                            }
+                        }
+                        mire += 10;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                    case 'V':
+                        mire += 5;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                    case 'I':
+                        if (mit.Length > 1)
+                        {
+                            switch (mit[1])
+                            {
+                                case 'X':
+                                    mire += 9;
+                                    return Arabra(mit.Remove(0, 2), ref mire);
+                                case 'V':
+                                    mire += 4;
+                                    return Arabra(mit.Remove(0, 2), ref mire);
+                                default:
+                                    mire += 1;
+                                    return Arabra(mit.Remove(0, 1), ref mire);
+                            }
+                        }
+                        mire += 1;
+                        return Arabra(mit.Remove(0, 1), ref mire);
+                }
+            }
+            return mire;
+        }
+    }
+}
